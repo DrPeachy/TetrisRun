ed153b301a5575b98d32613027a79e986bbcf063
DrPeachy <p1067838263@gmail.com>
2022-06-05 21:20:20 +0800

    cat+dogs

--- Assets/Scripts/MovingHuman/HumanManager.cs
+++ Assets/Scripts/MovingHuman/HumanManager.cs
@@ -8,6 +8,8 @@ public class HumanManager : MonoBehaviour
     public static HumanManager Instance;
     //  human generate
     public GameObject humanPrefab;
+    public GameObject catPrefab;
+    public GameObject dogPrefab;
     public Transform generateLeftMost; // left most human position
     public float intervalLength; // interval between each human
     public int pathNumber = 10; // same as human number
@@ -24,27 +26,59 @@ public class HumanManager : MonoBehaviour
     }
 
     private void Start() {
-        GenerateHuman();
+        GenerateCreature();
         //StartCoroutine(ResetHuman());
         scoreText.text = $"{DataManager.Instance.playerName}'s score: {DataManager.Instance.playerScore}";
         
     }
-    void GenerateHuman(){
+    void GenerateCreature(){
         //  clear out global height list
         PubVar.humanHeights = new List<int>(new int[pathNumber]);
         humans = new GameObject[pathNumber];
         humanCodes = new HumanCode[pathNumber];
 
         //  generate huamns
+        int humanNum = 0;
         for(int i = 0; i < pathNumber; i++){
-            Debug.Log($"create human number.{i}");
-            GameObject newHuman = Instantiate(humanPrefab, generateLeftMost.position + i * Vector3.right * intervalLength, Quaternion.identity);
-            newHuman.GetComponent<HumanCode>().index = i;
-            humans[i] = newHuman;
-            humanCodes[i] = newHuman.GetComponent<HumanCode>();
+            int flag = Random.Range(0, 3);
+            GameObject creatureToGenerate;
+            switch(flag){
+                case 0:
+                    if(humanNum < 2){
+                        creatureToGenerate = humanPrefab;
+                        humanNum++;
+                    }
+                    else{
+                        creatureToGenerate = catPrefab;
+                        flag = 1;
+                    }
+                    break;
+                case 1:
+                    creatureToGenerate = catPrefab;
+                    break;
+                case 2:
+                    creatureToGenerate = dogPrefab;
+                    break;
+                default:
+                    creatureToGenerate = null;
+                    break;
+
+            }
+            GameObject newCreature = Instantiate(creatureToGenerate, generateLeftMost.position + i * Vector3.right * intervalLength, creatureToGenerate.transform.rotation);
+            humans[i] = newCreature;
+            humanCodes[i] = newCreature.GetComponent<HumanCode>();
+            switch(flag){
+                case 0:
+                    humanCodes[i].height = 2;
+                    break;
+                default:
+                    humanCodes[i].height = 1;
+                    break;
+            }
+            humanCodes[i].index = i;
 
             //  set height to global
-            PubVar.humanHeights[i] = newHuman.GetComponent<HumanCode>()._humanScale.height;
+            PubVar.humanHeights[i] = humanCodes[i].height;
 
             //  check gameover
             InvokeRepeating("CheckGameOver", 3f, 0.2f);
@@ -69,7 +103,7 @@ public class HumanManager : MonoBehaviour
         int sum = 0;
         for(int i = 0; i < pathNumber; i++){
             if(humans[i] != null){
-                sum += humanCodes[i]._humanScale.height * (int)(100f * multiplier);
+                sum += humanCodes[i].height * (int)(100f * multiplier);
             }
         }
         DataManager.Instance.playerScore += sum;
